f887c110a636a8e9554e693fe87fd86b0cc22858
zephyr724 <zephyr724@gmail.com>
2026-04-13 20:10:58 +1200

    modify listen host and post, delete baseURL config

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3030);
+  const host = process.env.HOST || '0.0.0.0';
+  const port = process.env.PORT || 3030;
+  await app.listen(port, host);
 }
 bootstrap();
