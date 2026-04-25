65645007c4d63e7fc6d358ef487babdc29b34155
zephyr724 <zephyr724@gmail.com>
2026-04-25 19:35:02 +1200

    add dummy text and image for placeholder in sections

--- client/app/project/Sections.tsx
+++ client/app/project/Sections.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface Props {
   sections: {
     id: number;
@@ -24,17 +26,27 @@ const Sections = ({ sections }: Props) => {
 
   return (
     <div className="bg-gray-600 rounded text-white p-3">
-      <h2 className="text-lg font-semibold">Responsibilities</h2>
-      <div className="flex gap-x-2  pt-2">
-        {sections.map((section) => (
-          <div
-            className=" bg-gray-400 rounded px-2 min-w-10 whitespace-nowrap text-center"
-            key={section.id}
-          >
-            {section.title}
+      <h2 className="text-lg font-semibold">Implementations</h2>
+      {sections.map((section) => (
+        <div
+          className=" bg-gray-400 rounded px-2 w-full min-w-10 whitespace-nowrap text-left"
+          key={section.id}
+        >
+          <h3> {section.title}</h3>
+          <div>{section.contentText[0].text}</div>
+
+          <div className=" w-full h-auto">
+            <Image
+              src={section.contentImages[0].url}
+              alt=""
+              width={400}
+              height={300}
+              className="object-cover"
+            ></Image>
+            <div>{section.contentText[1].text}</div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
