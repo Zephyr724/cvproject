f69bea93b4d25425cd20778051cc000282e0f182
zephyr724 <zephyr724@gmail.com>
2026-04-29 17:47:21 +1200

    feat: add single project retrieval with includes in GET request

--- client/app/api/projects/[id]/route.tsx
+++ client/app/api/projects/[id]/route.tsx
@@ -1,6 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { number } from "zod";
+import { projectInclude } from "@/lib/project-includes";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  //
+  const { id } = await params;
+  const idNumber = parseInt(id);
+  const project = await prisma.project.findUnique({
+    where: { id: idNumber },
+    include: projectInclude,
+  });
+
+  if (!project)
+    return NextResponse.json({ error: "Project not found." }, { status: 400 });
+
+  return NextResponse.json(project, { status: 200 });
+}
 
 export async function PUT(
   request: NextRequest,
