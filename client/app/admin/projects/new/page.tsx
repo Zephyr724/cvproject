62a03a1f70a3234efd880bbccb446c5bb0146c3c
zephyr724 <zephyr724@gmail.com>
2026-05-03 21:30:41 +1200

    Feat: Add form component to admin project page

--- client/app/admin/projects/new/page.tsx
+++ client/app/admin/projects/new/page.tsx
@@ -1,7 +1,28 @@
+"use client";
 import ProjectDisplay from "@/app/components/ProjectDisplay";
 import { projectData } from "@/app/projects/[id]/mockData";
+import type { ValidateCreateProjectSchema } from "@/app/api/projects/schema";
+import { useRouter } from "next/dist/client/components/navigation";
+import { useState } from "react";
+import form from "react";
 
-const page = () => {
+const adminProjectPage = () => {
+  const INITIAL_FORM: ValidateCreateProjectSchema = {
+    title: "",
+    projectUrl: "",
+    githubUrl: "",
+    tags: [],
+    techItems: [],
+    roles: [],
+    sections: [],
+  };
+
+  const [formData, setFormData] =
+    useState<ValidateCreateProjectSchema>(INITIAL_FORM);
+  const router = useRouter();
+
+  const handleSubmit = () => {};
+
   return (
     <>
       <h1>New Projectpage</h1>
@@ -9,10 +30,23 @@ const page = () => {
         <div className="flex-1 overflow-auto max-h-[calc(100vh-2rem)] p-5">
           <ProjectDisplay project={projectData} showCloseButton={false} />
         </div>
-        <div className="flex-1">Edit area</div>
+        <div className="flex-1">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <div>
+              <label>Project Title</label>
+              <input className="input" name="query" />
+              <button type="submit">Submit</button>{" "}
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
 };
 
-export default page;
+export default adminProjectPage;
