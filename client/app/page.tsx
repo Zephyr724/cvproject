4b0eeea0dd579aee85dc95b11cb24099aa0f0105
zephyr724 <zephyr724@gmail.com>
2026-06-01 15:56:35 +1200

    Feature: refactor authentication handling by exporting authOptions and updating Home component to display user session on server

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,15 @@
+import { getServerSession } from "next-auth";
+import { getServerField } from "next/dist/server/lib/render-server";
 import Link from "next/link";
+import { authOptioins } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptioins);
+
   return (
     <div className="w-full">
       <main>
-        <Link href="/projects">Project</Link>
+        <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       </main>
     </div>
   );
