51e0c34d7d1844a1ef73f34ead8e370e9fb9797c
zephyr724 <zephyr724@gmail.com>
2026-06-08 23:07:31 +1200

    feat: enhance SectionEditorToolBar with dynamic tool buttons

--- client/app/admin/_components/SectionEditorToolBar.tsx
+++ client/app/admin/_components/SectionEditorToolBar.tsx
@@ -4,9 +4,26 @@ interface Props {
   editor: Editor;
 }
 
-const SectionEditorToolBar = ({editor}:Props) => {
+const SectionEditorToolBar = ({ editor }: Props) => {
+  const ToolsConfig = [
+    { label: "B", action: "toggleBold", title: "Bold Ctrl+B" },
+  ];
+
   return (
     <div className="border border-gray-300 rounded bg-base-100 p-1 ">
+      {ToolsConfig.map((tool) => (
+        <button
+          key={tool.label}
+          title={tool.title}
+          className="btn btn-sm btn-ghost border hover:border-black-200 hover:bg-black-100"
+          onClick={() => {
+            const command = editor.chain().focus();
+            const action = command[tool.action as keyof typeof command];
+          }}
+        >
+          B
+        </button>
+      ))}
       <button
         key={"bold"}
         title="Bold"
