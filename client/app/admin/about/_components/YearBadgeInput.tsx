fc01ff815d04b1bad76c3c817e82f6b535b7d4f9
Tiffani <93360736+yan929@users.noreply.github.com>
2026-07-15 15:13:50 +1200

    Feature: add a funtion to show "Currently working here"

--- client/app/admin/about/_components/YearBadgeInput.tsx
+++ client/app/admin/about/_components/YearBadgeInput.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
 function YearBadgeInput() {
+  const [isCurrentlyWorking, setIsCurrentlyWorking] = useState(false);
+
   return (
     <div className="bg-neutral text-neutral-content rounded-xl px-4 py-2">
       <div className="grid grid-cols-2 gap-4">
@@ -6,16 +12,34 @@ function YearBadgeInput() {
           <p className="mb-1 text-sm">Start Date</p>
           <input
             type="date"
-            className="input input-bordered w-full rounded-md"
+            className="input input-bordered w-full rounded-md text-primary"
           />
         </label>
 
         <label>
-          <p className="mb-1 text-sm">End Date</p>
+          <p className="mb-1 text-sm ">End Date</p>
+          {isCurrentlyWorking ? (
+            <p className="text-md font-semibold border border-white border-radius rounded-md px-2 py-2 text-center">
+              Present
+            </p>
+          ) : (
+            <input
+              type="date"
+              className="input input-bordered w-full rounded-md text-primary"
+              disabled={isCurrentlyWorking}
+            />
+          )}
+        </label>
+      </div>
+      <div>
+        <label className="flex items-center gap-2 mt-2">
           <input
-            type="date"
-            className="input input-bordered w-full rounded-md"
+            type="checkbox"
+            className="checkbox checkbox-sm checkbox-primary border-2 border-secondary"
+            checked={isCurrentlyWorking}
+            onChange={(e) => setIsCurrentlyWorking(e.target.checked)}
           />
+          <span className="text-sm">Currently working here</span>
         </label>
       </div>
     </div>
