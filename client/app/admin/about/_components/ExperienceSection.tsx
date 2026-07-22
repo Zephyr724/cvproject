fb89e87882dc04147852d83faa6a5f1c777de8ea
Tiffani <93360736+yan929@users.noreply.github.com>
2026-07-22 13:24:41 +1200

    Feature: update the import file name in experience section

--- client/app/admin/about/_components/ExperienceSection.tsx
+++ client/app/admin/about/_components/ExperienceSection.tsx
@@ -1,26 +1,32 @@
 "use client";
 
 import { Button } from "@radix-ui/themes/components/index";
-import NewExperienceForm from "./NewExperienceForm";
+import ExperienceFormContent from "./ExperienceForm";
 import Link from "next/link";
 import { Experience } from "@/app/admin/about/types";
 import ExperienceCard from "@/app/components/ExperienceCard";
 import { useState } from "react";
 
 function ExperienceSection({ experiences }: { experiences: Experience[] }) {
-  const [showNewExperienceForm, setShowNewExperienceForm] = useState(false);
+  const [showExperienceFormContent, setShowExperienceFormContent] =
+    useState(false);
   return (
     <>
       <h1>Experience</h1>
 
       <div className="my-2">
-        <Button color="green" onClick={() => setShowNewExperienceForm(true)}>
+        <Button
+          color="green"
+          onClick={() => setShowExperienceFormContent(true)}
+        >
           New Experience
         </Button>
       </div>
 
-      {showNewExperienceForm && (
-        <NewExperienceForm onCancel={() => setShowNewExperienceForm(false)} />
+      {showExperienceFormContent && (
+        <ExperienceFormContent
+          onCancel={() => setShowExperienceFormContent(false)}
+        />
       )}
       {experiences?.map((experience) => (
         <ExperienceCard
