535451e1a1282614ce44b74a3f6977164db2c4f1
Tiffani <93360736+yan929@users.noreply.github.com>
2026-07-22 19:44:45 +1200

    Feature: Add a error handle service function and apply to experience.service.ts

--- client/lib/server/services/experience.service.ts
+++ client/lib/server/services/experience.service.ts
@@ -8,7 +8,7 @@ import {
   ValidateUpdateExperienceType,
 } from "@/app/api/experiences/validationSchema";
 import { Prisma } from "@/src/generated/prisma/client";
-import error from "next/dist/api/error";
+import { handlePrismaError } from "./errorHandle.service";
 
 export const experienceService = {
   async getAllExperiences() {
@@ -154,29 +154,4 @@ function toPrismaUpdateExperienceInput(
   }
 
   return updateData;
-}
-
-function handlePrismaError(error: unknown): never {
-  if (error instanceof Prisma.PrismaClientKnownRequestError) {
-    if (error.code === "P2025") {
-      throw new BusinessError(
-        `Related record not found: ${error.meta?.cause || "please check the IDs in tags / techItems / roles / content"}`,
-        422,
-      );
-    }
-    if (error.code === "P2002") {
-      throw new BusinessError(
-        "A record with that unique value already exists",
-        409,
-      );
-    }
-    if (error.code === "P2014") {
-      // The change you are trying to make would violate the required relation
-      throw new BusinessError(
-        `Cannot delete: related records still exist. Remove them first.`,
-        409,
-      );
-    }
-  }
-  throw error; // unknown error → let global error handler handle → 500
 }
